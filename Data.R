c175bfe46883ee2562a22557cb463dcfa322d5ab
VictorMUribe <111189859+VictorMUribe@users.noreply.github.com>
2023-03-04 12:20:03 -0600

    changing everything

--- Data.R
+++ Data.R
@@ -8,15 +8,15 @@ library(tidymodels)
 
 
 
-
+lf_ump_name, rf_ump_name, saving_pitcher_name
 
 set.seed(123)
-GameLogs <- data.table::fread("GameLogs.csv") %>% 
-  sample_n(15000) %>% 
-  select(-contains("ID")) %>% 
+GameLogs <- data.table::fread("/Users/victoruribe/Documents/GameLogs.csv") %>% 
+  sample_n(25000) %>% 
+  select(-contains(c("ID", "name"))) %>% 
   janitor::clean_names() %>% 
   select(-c(completition_information, forfeit_information, protest_information, 
-            lf_ump_name, rf_ump_name, additional_information, acquisition_information)) %>% 
+            additional_information, acquisition_information)) %>% 
   mutate(
     date = lubridate::ymd(date),
     dayof_week = factor(dayof_week),
@@ -25,39 +25,60 @@ GameLogs <- data.table::fread("GameLogs.csv") %>%
     home_team = factor(home_team),
     home_team_league = factor(home_team_league),
     day_night = factor(day_night),
-    home_plate_ump_name = factor(home_plate_ump_name),
-    x1b_ump_name = factor(x1b_ump_name),
-    x2b_ump_name = factor(x2b_ump_name),
-    x3b_ump_name = factor(x3b_ump_name),
-    visiting_team_manager_name = factor(visiting_team_manager_name),
-    home_team_manager_name = factor(home_team_manager_name),
-    winning_pitcher_name = factor(winning_pitcher_name),
-    losing_pitcher_name = factor(losing_pitcher_name),
-    saving_pitcher_name = factor(saving_pitcher_name),
-    game_winning_rbi_batter_name = factor(game_winning_rbi_batter_name),
-    visiting_team_starting_pitcher_name = factor(visiting_team_starting_pitcher_name),
-    home_team_starting_pitcher_name = factor(home_team_starting_pitcher_name),
-    visiting_team_player1_name = factor(visiting_team_player1_name),
-    visiting_team_player2_name = factor(visiting_team_player2_name),
-    visiting_team_player3_name = factor(visiting_team_player3_name),
-    visiting_team_player4_name = factor(visiting_team_player4_name),
-    visiting_team_player5_name = factor(visiting_team_player5_name),
-    visiting_team_player6_name = factor(visiting_team_player6_name),
-    visiting_team_player7_name = factor(visiting_team_player7_name),
-    visiting_team_player8_name = factor(visiting_team_player8_name),
-    visiting_team_player9_name = factor(visiting_team_player9_name),
-    home_team_player1_name = factor(home_team_player1_name),
-    home_team_player2_name = factor(home_team_player2_name),
-    home_team_player3_name = factor(home_team_player3_name),
-    home_team_player4_name = factor(home_team_player4_name),
-    home_team_player5_name = factor(home_team_player5_name),
-    home_team_player6_name = factor(home_team_player6_name),
-    home_team_player7_name = factor(home_team_player7_name),
-    home_team_player8_name = factor(home_team_player8_name),
-    home_team_player9_name = factor(home_team_player9_name)
+    #home_plate_ump_name = factor(home_plate_ump_name),
+    #x1b_ump_name = factor(x1b_ump_name),
+    #x2b_ump_name = factor(x2b_ump_name),
+    #x3b_ump_name = factor(x3b_ump_name),
+    #visiting_team_manager_name = factor(visiting_team_manager_name),
+    #home_team_manager_name = factor(home_team_manager_name),
+    #winning_pitcher_name = factor(winning_pitcher_name),
+    #losing_pitcher_name = factor(losing_pitcher_name),
+    #saving_pitcher_name = factor(saving_pitcher_name),
+    #game_winning_rbi_batter_name = factor(game_winning_rbi_batter_name),
+    #visiting_team_starting_pitcher_name = factor(visiting_team_starting_pitcher_name),
+    #home_team_starting_pitcher_name = factor(home_team_starting_pitcher_name),
+    #visiting_team_player1_name = factor(visiting_team_player1_name),
+    #visiting_team_player2_name = factor(visiting_team_player2_name),
+    #visiting_team_player3_name = factor(visiting_team_player3_name),
+    #visiting_team_player4_name = factor(visiting_team_player4_name),
+    #visiting_team_player5_name = factor(visiting_team_player5_name),
+    #visiting_team_player6_name = factor(visiting_team_player6_name),
+    #visiting_team_player7_name = factor(visiting_team_player7_name),
+    #visiting_team_player8_name = factor(visiting_team_player8_name),
+    #visiting_team_player9_name = factor(visiting_team_player9_name),
+    visiting_team_player1_position = factor(visiting_team_player1_position),
+    visiting_team_player2_position = factor(visiting_team_player2_position),
+    visiting_team_player3_position = factor(visiting_team_player3_position),
+    visiting_team_player4_position = factor(visiting_team_player4_position),
+    visiting_team_player5_position = factor(visiting_team_player5_position),
+    visiting_team_player6_position = factor(visiting_team_player6_position),
+    visiting_team_player7_position = factor(visiting_team_player7_position),
+    visiting_team_player8_position = factor(visiting_team_player8_position),
+    visiting_team_player9_position = factor(visiting_team_player9_position),
+    #home_team_player1_name = factor(home_team_player1_name),
+    #home_team_player2_name = factor(home_team_player2_name),
+    #home_team_player3_name = factor(home_team_player3_name),
+    #home_team_player4_name = factor(home_team_player4_name),
+    #home_team_player5_name = factor(home_team_player5_name),
+    #home_team_player6_name = factor(home_team_player6_name),
+    #home_team_player7_name = factor(home_team_player7_name),
+    #home_team_player8_name = factor(home_team_player8_name),
+    #home_team_player9_name = factor(home_team_player9_name),
+    home_team_player1_position = factor(home_team_player1_position),
+    home_team_player2_position = factor(home_team_player2_position),
+    home_team_player3_position = factor(home_team_player3_position),
+    home_team_player4_position = factor(home_team_player4_position),
+    home_team_player5_position = factor(home_team_player5_position),
+    home_team_player6_position = factor(home_team_player6_position),
+    home_team_player7_position = factor(home_team_player7_position),
+    home_team_player8_position = factor(home_team_player8_position),
+    home_team_player9_position = factor(home_team_player9_position)
   )
 
 
+# replaces (none) with NA as it should
+GameLogs[,2:ncol(GameLogs)] <- as.data.frame(GameLogs[,2:ncol(GameLogs)]) %>%
+  mutate_all(~na_if(., "(none)"))
 
 
 #GameLogs %>% 
@@ -76,11 +97,11 @@ na_prop <- function(col){
 
 
 
+
 data.frame(prop = sapply(data.frame(GameLogs), na_prop) ) %>% 
   select(prop) %>% 
-  filter(prop <= .1) %>% 
-  top_n(2)# note that putting more than 2 results in the inclusion
-#of every variable since 0 is the next lowest
+  filter(prop > 0)
+
 
 #prop
 #completition_information 0.999448
@@ -90,8 +111,8 @@ data.frame(prop = sapply(data.frame(GameLogs), na_prop) ) %>%
 
 
 
-set.seed(123)
-gamelog_split <- initial_split(GameLogs, strata = attendance)
+set.seed(123)# note that 2:119 removes the dates
+gamelog_split <- initial_split(GameLogs[, 2:ncol(GameLogs)], strata = attendance)
 
 gamelog_train <- training(gamelog_split)
 gamelog_test <- testing(gamelog_split)
@@ -103,13 +124,7 @@ gamelog_fold <- vfold_cv(gamelog_train, v = 10)
 
 
 
-
-
-
-
-
-
-
+gamelog_train$numberof_games^2 %>% density() %>% plot()
 
 
 
